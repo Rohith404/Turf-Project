2c32e0379b536f812633ae42a0204d5c26f059ec
Rohith <talk.rohith004@gmail.com>
2023-07-20 15:31:51 +0530

    Updated

--- turf_project/iddo/settings.py
+++ turf_project/iddo/settings.py
@@ -28,6 +28,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'turfapp',
     'crispy_forms',
+    'crispy_bootstrap5',
     'forms_fieldset',
 ]
 
@@ -46,7 +47,10 @@ ROOT_URLCONF = 'iddo.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'turfapp/templates'),
+            os.path.join(BASE_DIR, 'turfapp/templates/turf'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -96,7 +100,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 STATIC_URL = '/static/'
@@ -105,7 +108,7 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CRISPY_TEMPLATE_PACK = 'bootstrap3'
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
